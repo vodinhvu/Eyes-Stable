3923548b92a3cff65c3429031927e05e94e44c0d
vodinhvu <vodinhvu222@gmail.com>
2023-09-11 22:05:52 +0700

    change format train, add metris function

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import time
 import torch
 import tqdm
 from torchvision import transforms, datasets, models
+import torchmetrics
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -46,7 +47,7 @@ if __name__ == '__main__':
     model_ft = models.resnet18(weights='IMAGENET1K_V1')
 
     num_features = model_ft.fc.in_features
-    model_ft.fc = torch.nn.Sequential(torch.nn.Linear(num_features, len(class_names)), torch.nn.Softmax())
+    model_ft.fc = torch.nn.Sequential(torch.nn.Linear(num_features, len(class_names)), torch.nn.Softmax(dim=1))
     model_ft = model_ft.to(device)
 
     i = 0
@@ -76,61 +77,64 @@ if __name__ == '__main__':
     torch.save(model_ft.state_dict(), best_model_params_path)
     best_acc = 0.0
 
+    conf_mat_fn = torchmetrics.ConfusionMatrix(task='binary', num_classes=2)
+    accuracy_fn = torchmetrics.Accuracy(task='binary', num_classes=2)
+
     for epoch in range(opt.epochs):
         print(f'Epoch {epoch}/{opt.epochs - 1}')
         print('-' * 10)
 
-        # Each epoch has a training and validation phase
-        for phase in ['train', 'val']:
-            if phase == 'train':
-                model_ft.train()  # Set model to training mode
-            else:
-                model_ft.eval()  # Set model to evaluate mode
+        # Train
+        model_ft.train()
+        process = tqdm.tqdm(dataloaders['train'], desc='Train')
+        for inputs, labels in process:
+            inputs = inputs.to(device)
+            labels = labels.to(device)
+            # zero the parameter gradients
+            optimizer_ft.zero_grad()
+            # forward
+            # track history if only in train
+            with torch.set_grad_enabled(True):
+                outputs = model_ft.forward(inputs)
+                _, preds = torch.max(outputs, 1)
+                loss = criterion(outputs, labels)
 
-            running_loss = 0.0
-            running_corrects = 0
+                # backward + optimize only if in training phase
 
-            process = tqdm.tqdm(dataloaders[phase], desc=phase)
-            # Iterate over data.
-            for inputs, labels in process:
-                inputs = inputs.to(device)
-                labels = labels.to(device)
+                loss.backward()
+                optimizer_ft.step()
+            process.set_postfix({'loss': loss.item()})
 
-                # zero the parameter gradients
-                optimizer_ft.zero_grad()
+        # Validation
+        model_ft.eval()
+        process = tqdm.tqdm(dataloaders['val'], desc='Validation')
+        all_preds = []
+        all_labels = []
+        for inputs, labels in process:
+            inputs = inputs.to(device)
+            labels = labels.to(device)
+            with torch.set_grad_enabled(False):
+                outputs = model_ft.forward(inputs)
+                accuracy = accuracy_fn(outputs, labels)
+                all_preds.append(outputs.tolist())
+                all_labels.append(labels.tolist())
+                process.set_postfix({'accuracy': accuracy.item()})
+        all_preds_tensors = torch.tensor(all_preds, device=device)
+        all_labels_tensors = torch.tensor(all_labels, device=device)
+        all_accuracy = accuracy_fn(all_preds_tensors, all_labels_tensors).item()
+        conf_mat = conf_mat_fn(all_preds_tensors, all_labels_tensors)
 
-                # forward
-                # track history if only in train
-                with torch.set_grad_enabled(phase == 'train'):
-                    outputs = model_ft.forward(inputs)
-                    _, preds = torch.max(outputs, 1)
-                    loss = criterion(outputs, labels)
+        torch.save(model_ft.state_dict(), os.path.join(opt.save, f'epoch_{epoch}_acc_{all_accuracy:.4f}.pt'))
 
-                    # backward + optimize only if in training phase
-                    if phase == 'train':
-                        loss.backward()
-                        optimizer_ft.step()
+        if all_accuracy > best_acc:
+            best_acc = all_accuracy
+            torch.save(model_ft.state_dict(), best_model_params_path)
 
-                # statistics
-                running_loss += loss.item() * inputs.size(0)
-                running_corrects += torch.sum(preds == labels.data)
-                process.set_postfix({'loss': loss.item()})
+        print(conf_mat)
 
-            epoch_loss = running_loss / dataset_sizes[phase]
-            epoch_acc = running_corrects.double() / dataset_sizes[phase]
-
-            print(f'{phase} loss: {epoch_loss:.4f} Acc: {epoch_acc:.4f}')
-
-            # deep copy the model
-            if phase == 'val':
-                torch.save(model_ft.state_dict(), os.path.join(opt.save, f'epoch_{epoch}_acc_{epoch_acc:.4f}.pt'))
-                if epoch_acc > best_acc:
-                    best_acc = epoch_acc
-                    torch.save(model_ft.state_dict(), best_model_params_path)
-
-        exp_lr_scheduler.step()
         time_elapsed = time.time() - since
         print(f'Training complete in {time_elapsed // 60:.0f}m {time_elapsed % 60:.0f}s')
+        exp_lr_scheduler.step()
         print()
 
     print(f'Best val Acc: {best_acc:4f}')
